7e8faa32b622a4722462537f9d1545d837e1038a
kshitij3 <53857127+kshitij3@users.noreply.github.com>
2020-01-30 12:49:03 +0530

    Update search.c

--- search.c
+++ search.c
@@ -1,3 +1,44 @@
+ANALYSIS AND DESIGN OF ALGORITHMS LAB
+
+Week 0-Assignment
+
+Problem:
+
+Given an sorted array arr[] of N integers and a number K is given. The task is to check if the element K is present in the array or not.
+
+Input:
+
+First line of input contains number of testcases T. For each testcase, first line of input contains number of elements in the array and the number K separated by space. Next line contains N elements.
+
+Output:
+
+For each testcase, if the element is present in the array print "1" (without quotes), else print "-1" (without quotes).
+
+Example:
+
+Input:
+
+2
+
+5 6
+
+1 2 3 4 6
+
+5 2
+
+1 3 4 5 6
+
+Output:
+
+1
+
+-1
+
+Explanation:
+
+Testcase 1: Since, 6 is present in the array at index 4 (0-based indexing), so output is 1.
+
+Testcase 2: Since, 2 is not present in the array, so output is -1.
 #include<stdio.h>
 #include<stdlib.h>
 
